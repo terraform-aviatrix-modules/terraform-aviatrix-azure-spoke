0529d2a619fe1f355dea2a68881e8d9c5e3bd0d3
Dennizz <root@ipaddr.nl>
2020-09-05 16:42:12 +0200

    formatting

--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 variable "name" {
   description = "Custom name for VNETs, gateways, and firewalls"
-  type = string
+  type        = string
 }
 
 variable "region" {
   description = "The Azure region to deploy this module in"
-  type = string
+  type        = string
 }
 
 variable "cidr" {
   description = "The CIDR range to be used for the VNET"
-  type = string
+  type        = string
 }
 
 variable "account" {
   description = "The Azure account name, as known by the Aviatrix controller"
-  type = string
+  type        = string
 }
 
 variable "instance_size" {
   description = "Azure Instance size for the Aviatrix gateways"
-  type    = string
-  default = "Standard_B1ms"
+  type        = string
+  default     = "Standard_B1ms"
 }
 
 variable "ha_gw" {
   description = "Boolean to determine if module will be deployed in HA or single mode"
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "active_mesh" {
   description = "Enables Aviatrix active mesh"
-  type    = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "transit_gw" {
   description = "Transit gateway to attach spoke to"
-  type = string
+  type        = string
 }
 
 variable "insane_mode" {
